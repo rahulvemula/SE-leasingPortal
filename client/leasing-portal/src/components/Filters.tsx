7f9eb0264520398e8a5e841b345582c9c220f182
R V <rahul.vemula50@gmail.com>
2022-02-01 02:26:21 -0500

    feat #42: Bare UI for home page

---
+++ client/leasing-portal/src/components/Filters.tsx
@@ -0,0 +1,98 @@
+function Filters() {
+    return (
+        <>
+            <section className="ftco-section ftco-no-pb">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-12">
+                            <div className="search-wrap-1">
+                                <form action="#" className="search-property-1">
+                                    <div className="row">
+                                        <div className="col-lg align-items-end">
+                                            <div className="form-group">
+                                                <label htmlFor="#">Location</label>
+                                                <div className="form-field">
+                                                    <div className="icon"><span className="ion-ios-search"></span></div>
+                                                    <input type="text" className="form-control" placeholder="City/Locality Name" />
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div className="col-lg align-items-end">
+                                            <div className="form-group">
+                                                <label htmlFor="#">Property Type</label>
+                                                <div className="form-field">
+                                                    <div className="select-wrap">
+                                                        <div className="icon"><span className="ion-ios-arrow-down"></span></div>
+                                                        <select name="" id="" className="form-control">
+                                                            <option value="">Type</option>
+                                                            <option value="">Commercial</option>
+                                                            <option value="">- Office</option>
+                                                            <option value="">Residential</option>
+                                                            <option value="">Villa</option>
+                                                            <option value="">Condominium</option>
+                                                            <option value="">Apartment</option>
+                                                        </select>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div className="col-lg align-items-end">
+                                            <div className="form-group">
+                                                <label htmlFor="#">Property Status</label>
+                                                <div className="form-field">
+                                                    <div className="select-wrap">
+                                                        <div className="icon"><span className="ion-ios-arrow-down"></span></div>
+                                                        <select name="" id="" className="form-control">
+                                                            <option value="">Type</option>
+                                                            <option value="">Rent</option>
+                                                            <option value="">Sale</option>
+                                                        </select>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div className="col-lg align-items-end">
+                                            <div className="form-group">
+                                                <label htmlFor="#">Price Limit</label>
+                                                <div className="form-field">
+                                                    <div className="select-wrap">
+                                                        <div className="icon"><span className="ion-ios-arrow-down"></span></div>
+                                                        <select name="" id="" className="form-control">
+                                                            <option value="">$5,000</option>
+                                                            <option value="">$10,000</option>
+                                                            <option value="">$50,000</option>
+                                                            <option value="">$100,000</option>
+                                                            <option value="">$200,000</option>
+                                                            <option value="">$300,000</option>
+                                                            <option value="">$400,000</option>
+                                                            <option value="">$500,000</option>
+                                                            <option value="">$600,000</option>
+                                                            <option value="">$700,000</option>
+                                                            <option value="">$800,000</option>
+                                                            <option value="">$900,000</option>
+                                                            <option value="">$1,000,000</option>
+                                                            <option value="">$2,000,000</option>
+                                                        </select>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div className="col-lg align-self-end">
+                                            <div className="form-group">
+                                                <div className="form-field">
+                                                    <input type="submit" value="Search Property" className="form-control btn btn-primary" />
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default Filters;
---
+++ client/leasing-portal/src/components/Hero.tsx
@@ -0,0 +1,25 @@
+function Hero() {
+    return (
+        <>
+            <div className="hero-wrap" style={{ backgroundImage: 'url(images/home_main.webp)', backgroundPosition: '50% 0px' }} data-stellar-background-ratio="0.5">
+                <div className="overlay"></div>
+                <div className="overlay-2"></div>
+                <div className="container">
+                    <div className="row no-gutters slider-text justify-content-center align-items-center">
+                        <div className="col-lg-8 col-md-6 d-flex align-items-end">
+                            <div className="text text-center w-100">
+                                <h1 className="mb-4"><strong>Find Rentals</strong> <br />That Save You Money</h1>
+                            </div>
+                            <div className="mouse">
+                                <a href="#" className="mouse-icon">
+                                    <div className="mouse-wheel"><i className="bi bi-caret-down-fill"></i></div>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+export default Hero;
