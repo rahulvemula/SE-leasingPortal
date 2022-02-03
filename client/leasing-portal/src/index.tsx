c0ed25b3c6b5ececba893a011182e99c773819ec
Lahari Barad <baradlahari@gmail.com>
2022-02-02 23:40:42 -0500

    address review comments

--- client/leasing-portal/src/index.tsx
+++ client/leasing-portal/src/index.tsx
@@ -3,17 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Filters from './components/Filters';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="listing" element={<Filters />} />
-    </Routes>
-    </BrowserRouter>
+    <App/>
+    
   </React.StrictMode>,
   document.getElementById('root')
 );
