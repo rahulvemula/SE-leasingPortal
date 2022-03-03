c930f4f0990c20e99f0a4c0722df2de19e6f2b57
R V <rahul.vemula50@gmail.com>
2022-03-03 10:07:28 -0500

    test: added unit tests for app component #103

--- client/leasing-portal/src/App.test.tsx
+++ client/leasing-portal/src/App.test.tsx
@@ -2,8 +2,16 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Hous logo', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const housElement = screen.getByText('HOUS.');
+  expect(housElement).toBeInTheDocument();
+});
+
+test('renders login and register buttons', () => {
+  render(<App />);
+  const loginElement = screen.getByText('Login');
+  expect(loginElement).toBeInTheDocument();
+  const registerElement = screen.getByText('Register');
+  expect(registerElement).toBeInTheDocument();
 });
