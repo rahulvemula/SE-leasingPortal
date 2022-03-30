8c35be0b3b5f61e21f20b8b87d1211cc69153503
R V <rahul.vemula50@gmail.com>
2022-03-29 23:23:46 -0400

    Updated mock data to contain id

--- client/leasing-portal/src/pages/ApptListing.tsx
+++ client/leasing-portal/src/pages/ApptListing.tsx
@@ -6,56 +6,64 @@ function ApptListing() {
             label: 'Full house - 4bhk',
             rent: 2509,
             sqft: 2400,
-            img: 'House1'
+            img: 'House1',
+            id: 1
     
         },
         {
             label: 'Full house - 3bhk',
             rent: 1500,
             sqft: 1800,
-            img: 'House2'
+            img: 'House2',
+            id: 2,
     
         },
         {
             label: 'Single room in 3 Bed 2 Bath (Shared)',
             rent: 509,
             sqft: 400,
-            img: '3bhk'
+            img: '3bhk',
+            id: 3
     
         },
         {
             label: '3 Bed 2 Bath (Private)',
             rent: 549,
             sqft: 400,
-            img: '3bhk'
+            img: '3bhk',
+            id:4
     
         },
         {
             label: '4 Bed 3 Bath (Shared)',
             rent: 449,
             sqft: 350,
-            img: '4bhk'
+            img: '4bhk',
+            id:5
     
         },
         {
             label: '4 Bed 3 Bath (Private)',
             rent: 489,
             sqft: 350,
-            img: '4bhk'
+            img: '4bhk',
+            id:6
     
         },
         {
             label: '4 Bed 4 Bath (Personal)',
             rent: 469,
             sqft: 350,
-            img: '4bhkP'
+            img: '4bhkP',
+            id:7
     
         },
         {
             label: '4 Bed 4 Bath (Private)',
             rent: 469,
             sqft: 350,
-            img: '4bhkP'
+            img: '4bhkP',
+            id:8
     
         }
 
