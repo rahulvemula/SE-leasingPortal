6b2995ffb00b5b634f437cf290201076e3e59a48
Mitul Mandaliya <mandaliyamitul@gmail.com>
2022-03-30 12:32:27 -0400

    Added test cases for complaint API

--- server/cmd/main/main.go
+++ server/cmd/main/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/common/easy-lease/pkg/routes"
 	"github.com/gorilla/handlers"
@@ -29,7 +30,7 @@ func main() {
 
 	r.HandleFunc("/", RootEndPoint).Methods("GET")
 
-	// port := os.Getenv("PORT")
+	port := os.Getenv("PORT")
 	routes.RegisterUserRoutes(r)
 	routes.RegisterLeaseRoutes(r)
 	routes.RegisterApartmentRoutes(r)
@@ -38,7 +39,7 @@ func main() {
 	routes.RegisterComplaintRoutes(r)
 
 	http.Handle("/", r)
-	log.Fatal(http.ListenAndServe("localhost:9010", handlers.CORS(corsObj, headersOk, methodsOk, allowCreds, allowOptions)(r)))
-	// log.Fatal(http.ListenAndServe(":"+port, handlers.CORS(corsObj, headersOk, methodsOk, allowCreds, allowOptions)(r)))
+	// log.Fatal(http.ListenAndServe("localhost:9010", handlers.CORS(corsObj, headersOk, methodsOk, allowCreds, allowOptions)(r)))
+	log.Fatal(http.ListenAndServe(":"+port, handlers.CORS(corsObj, headersOk, methodsOk, allowCreds, allowOptions)(r)))
 
 }
--- server/pkg/routes/complaint-routes.go
+++ server/pkg/routes/complaint-routes.go
@@ -10,5 +10,5 @@ var RegisterComplaintRoutes = func(router *mux.Router) {
 	router.Handle("/complaints", controllers.IsAuthorized(controllers.GetComplaints)).Methods("GET")
 	router.Handle("/complaints/{complaintId}", controllers.IsAuthorized(controllers.GetComplaintById)).Methods("GET")
 	router.Handle("/complaints/user/{userId}", controllers.IsAuthorized(controllers.GetComplaintsByUser)).Methods("GET")
-	router.Handle("/complaints/{complaintId}", controllers.IsAuthorized(controllers.DeleteApartment)).Methods("DELETE")
+	router.Handle("/complaints/{complaintId}", controllers.IsAuthorized(controllers.DeleteComplaint)).Methods("DELETE")
 }
