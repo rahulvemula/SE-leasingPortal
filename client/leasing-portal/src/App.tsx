846614b9a099fcfa8402096d01a470205699615d
R V <rahul.vemula50@gmail.com>
2022-04-18 22:29:24 -0400

    Fixed unit test failures in App component #201

--- client/leasing-portal/src/App.tsx
+++ client/leasing-portal/src/App.tsx
@@ -9,53 +9,63 @@ import ApptListing from "./pages/ApptListing";
 import LeaseConfirmation from "./pages/lease-confirmation";
 import { store } from "./store";
 import { Provider } from "react-redux";
-import AboutUs from './components/AboutUs/AboutUs';
+import AboutUs from "./components/AboutUs/AboutUs";
 
-import Upload from './components/UploadImage/UploadImage';
+import Upload from "./components/UploadImage/UploadImage";
 import Account from "./pages/Accounts";
 import Listing from "./pages/Listing";
-import Support from './pages/Support';
-import Terms from './pages/Terms';
+import Support from "./pages/Support";
+import Terms from "./pages/Terms";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
+  const Application = () => {
+    return (
+      <>
+        <Header />
+        <Routes>
+          <Route
+            path="*"
+            element={
+              <section className="ftco-section">
+                <span>not found</span>
+              </section>
+            }
+          />
+          <Route
+            path="/"
+            element={
+              <>
+                <Hero />
+                <Properties />
+                <Listings />
+              </>
+            }
+          />
+
+          <Route path="/support" element={<Support />} />
+          <Route path="/appts" element={<ApptListing />} />
+          <Route path="/lease-confirmation" element={<LeaseConfirmation />} />
+          <Route path="/aboutus" element={<AboutUs />} />
+          <Route path="/upload" element={<Upload />} />
+          {/* <Route path="/account" element = {<Account/>} /> */}
+          <Route path="/account" element={<Account />} />
+          <Route path="/listing/:id" element={<Listing />} />
+          <Route path="/terms" element={<Terms />} />
+        </Routes>
+        <Footer />
+      </>
+    );
+  };
   return (
     <Provider store={store}>
       <div className="App">
         <BrowserRouter basename="/SE-leasingPortal">
-          <Header />
-          <Routes>
-            <Route
-              path="*"
-              element={
-                <section className="ftco-section">
-                  <span>not found</span>
-                </section>
-              }
-            />
-            <Route
-              path="/"
-              element={
-                <>
-                  <Hero />
-                  <Properties />
-                  <Listings />
-                </>
-              }
-            />
-
-            <Route path="/support" element={<Support />} />
-            <Route path="/appts" element={<ApptListing />} />
-            <Route path="/lease-confirmation" element={<LeaseConfirmation />} />
-            <Route path="/aboutus" element={<AboutUs />} />
-            <Route path="/upload" element={<Upload/>} />
-            {/* <Route path="/account" element = {<Account/>} /> */}
-            <Route path="/account" element = {<Account/>} />
-            <Route path="/listing/:id" element = {<Listing/>} />
-            <Route path="/terms" element = {<Terms/>} />
-          </Routes>
-          <Footer />
+          <Application/>
+        </BrowserRouter>
+        <BrowserRouter>
+          <Application/>
         </BrowserRouter>
       </div>
     </Provider>
